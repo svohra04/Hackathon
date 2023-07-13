5ea87879aae3be5d655517e9f276a699719d8581
bgc7753 <bgc7753@g.rit.edu>
2023-07-13 16:57:46 -0400

    Add api validation for request body

--- server/routes/getEmployees.js
+++ server/routes/getEmployees.js
@@ -4,13 +4,22 @@ let Employees = require('../schemas/Employees')
 
 
 router.post('/', async(req, res)=>{
+
+    const validSearchParams = new Set(["Name","EmployeeNumber"])
+
     try{
         const searchParams = req.body;
+
+        for (let field in searchParams) {
+            if (!validSearchParams.has(field)) {
+                return res.status(400).send(`Invalid Search Body, Available Fields to Search on: [${Array.from(validSearchParams).join(", ")}]`);
+            }
+        }
         const response = await Employees.find(searchParams)
         res.json(response)
     }
     catch(err){
-        res.status(500).send(`Error getting employee: ${err.message}`);
+        return res.status(err.status || 500).send(`Error getting employee: ${err.message}`);
     }
 
 })
